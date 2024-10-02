c6e0eb5f089f9335d194c90a956d402b861dcac3
rhysqi <rhysqi.akbar@gmail.com>
2024-10-02 08:40:54 +0700

    Adding: - Adding System Metrics for get current screen resolution
    submit: rhysqi <rhysqi.akbar@gmail.com>

---
+++ Apps/Models/SystemMetricsModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ScreenBoard.Models;
+
+public class SystemMetricsModel
+{
+    [DllImport("user32.dll")]
+    private static extern int GetSystemMetrics(IntPtr hWnd);
+
+    private const int SM_CXSCREEN = 0;
+    private const int SM_CYSCREEN = 1;
+
+    int GetScreenWidht()
+    {
+        return GetSystemMetrics(SM_CXSCREEN);
+    }
+
+    int GetScreenHeight()
+    {
+        return GetSystemMetrics(SM_CYSCREEN);
+    }
+}
