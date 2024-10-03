8c769b68cdf102487b05935af2ea41be28fe5db3
rhysqi <rhysqi.akbar@gmail.com>
2024-10-03 20:22:42 +0700

    Updates: Adding Hotkeys - Adding HotKeys Model - Adding Register Hotkey Method - Adding HotKeys Definitions

---
+++ Apps/Models/RegisterHotKeysModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows;
+using System.Runtime.InteropServices;
+
+namespace ScreenBoard.Models;
+
+internal class RegisterHotKeysModel
+{
+    private const int WM_HOTKEY = 0x0312;
+
+    [DllImport("user32.dll")]
+    private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+    [DllImport("user32.dll")]
+    private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+    private const int HotkeyId = 9000; // Unique identifier for the hotkey
+
+    // Modifier keys
+    private const uint MOD_CONTROL = 0x0002;
+    private const uint MOD_SHIFT = 0x0004;
+
+    // Virtual key for 'O'
+    private const uint VK_O = 0x4F;
+}
