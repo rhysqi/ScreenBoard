8babfc7ba25f4154689033145da6d736eb89c4a7
rhysqi <rhysqi.akbar@gmail.com>
2024-12-07 15:45:43 +0700

    Fix: Clear Region - Fix region area for clear all drawing region that already has a buffer value.
    submit: rhysqi

--- Core/lib/Core-Event.h
+++ Core/lib/Core-Event.h
@@ -6,6 +6,9 @@
 
 inline int ActionKeyDown(HWND hwnd, WPARAM wParam, HDC memoryHDC)
 {
+    int DrawRegionLimitX = GetSystemMetrics(SM_CXMAXIMIZED);
+    int DrawRegionLimitY = GetSystemMetrics(SM_CYMAXIMIZED);
+
     if(wParam == VK_ESCAPE)
     {
         PostQuitMessage(0);
@@ -14,7 +17,7 @@ inline int ActionKeyDown(HWND hwnd, WPARAM wParam, HDC memoryHDC)
     if(wParam == 0x43)
     {
         // Clear the drawing buffer
-        PatBlt(memoryHDC, 0, 0, GetSystemMetrics(SM_CXFULLSCREEN), GetSystemMetrics(SM_CYFULLSCREEN), WHITENESS);
+        PatBlt(memoryHDC, 0, 0, DrawRegionLimitX, DrawRegionLimitY, WHITENESS);
 
         // Invalidate the window to force a repaint
         InvalidateRect(hwnd, NULL, TRUE); // hWnd should be a global or passed in some way
